35482e85a97552a3634f7b031def607a0d1885b6
Chen Fang <mtdcy.chen@gmail.com>
2025-02-26 07:36:30 +0800

    update cmdlets.sh

--- install.sh
+++ install.sh
@@ -34,9 +34,7 @@ case "$1" in
         for x in "${archs[@]}"; do
             export CMDLETS_ARCH="$x"
             export CMDLETS_STRIP=0
-            ./cmdlets.sh install nvim
-            ./cmdlets.sh install ctags
-            ./cmdlets.sh install rg     # ripgrep
+            ./cmdlets.sh install nvim ctags rg
         done
         exit
         ;;
